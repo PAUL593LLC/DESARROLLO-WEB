4a7cb5d4ad06ff19a241bfa1637df70608dd9410
PAUL593LLC <ep.llerenac@uea.edu.ec>
2025-02-20 22:10:23 -0500

    TAREA SEMANA 9

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'BIENVENIDOS NUEVAMENTE AMIGOS'
+
+# NUEVA RUTA
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f"Hola, {nombre}!"
 
 
 if __name__ == '__main__':
